22ae05143bcb3af33094214027a3bcb75e7cac3a
alexphys <alexandros.mastas@gmail.com>
2014-09-02 12:23:34 +0300

    Update with black_list and white list methods controllers and actions and params

--- lib/logstasher/log_subscriber.rb
+++ lib/logstasher/log_subscriber.rb
@@ -4,26 +4,101 @@ require 'active_support/log_subscriber'
 module LogStasher
   class RequestLogSubscriber < ActiveSupport::LogSubscriber
     def process_action(event)
+      #unless event.payload[:action_black_list].include?('all') && event.payload[:action_white_list].blank?
+      #binding.pry
       payload = event.payload
 
-      data      = extract_request(payload)
-      data.merge! extract_status(payload)
-      data.merge! runtimes(event)
-      data.merge! location(event)
-      data.merge! extract_exception(payload)
-      data.merge! extract_custom_fields(payload)
+      make_black_list(payload)
 
-      tags = ['request']
-      tags.push('exception') if payload[:exception]
-      event = LogStash::Event.new(data.merge({'@source' => LogStasher.source, '@tags' => tags}))
-      LogStasher.logger << event.to_json + "\n"
+      unless forbidden(payload)
+        puts 'Logging ' + payload[:controller] + ' with action ' + payload[:action]
+        data      = extract_request(payload)
+        data.merge! extract_status(payload)
+        data.merge! runtimes(event)
+        data.merge! location(event)
+        data.merge! extract_exception(payload)
+        data.merge! extract_custom_fields(payload)
+  
+        tags = ['request']
+        tags.push('exception') if payload[:exception]
+        event = LogStash::Event.new(data.merge({'@source' => LogStasher.source, '@tags' => tags}))
+        LogStasher.logger << event.to_json + "\n"
+      else
+        puts 'rejected ' + payload[:controller] + ' with action ' + payload[:action]
+      end
     end
     
     def extract_custom_fields(payload)
-      custom_fields = (!LogStasher.custom_fields.empty? && payload.extract!(*LogStasher.custom_fields - [:action_black_list])) || {}
+
+      custom_fields = (!LogStasher.custom_fields.empty? && payload.extract!(*LogStasher.custom_fields - [:black_list] - [:white_list] - [:forbidden_params] - [:black_list_everything])) || {}
       LogStasher.custom_fields.clear
       custom_fields
-    end  
+    end 
+
+    def make_black_list(payload)
+      if payload[:black_list_everything]
+        payload[:black_list] = {method: [payload[:method]], controller: {payload[:controller] => {actions: [payload[:action]]}}}
+        # event.payload[:black_list][:method] = event.payload[:method]
+        # event.payload[:black_list][:controller] = event.payload[:controller]
+        # event.payload[:black_list][:action] = event.payload[:action]
+      end  
+    end 
+
+    def forbidden(payload)
+
+      payload[:white_list] ||= {method: [], controller: {}}  
+      payload[:black_list] ||= {method: [], controller: {}}
+      payload[:forbidden_params] ||= {}
+  
+      if payload[:black_list][:method].include?(payload[:method])
+
+        if (payload[:white_list][:controller][payload[:controller]] && payload[:white_list][:controller][payload[:controller]][:actions].include?(payload[:action])) or (payload[:white_list][:controller][payload[:controller]] == {actions: []}) or payload[:white_list][:method].include?(payload[:method])
+          reject = false
+
+        else
+          reject = true
+        end
+      end
+      reject ||=false
+
+
+      unless reject
+        if payload[:black_list][:controller]
+
+          reject = payload[:black_list][:controller].keys.include?(payload[:controller]) and !payload[:white_list][:controller].keys.include?(payload[:controller])
+          if reject
+            black_actions = payload[:black_list][:controller][payload[:controller]][:actions] || []
+            payload[:white_list][:controller][payload[:controller]] ||= {}
+            white_actions = payload[:white_list][:controller][payload[:controller]][:actions] || []
+            reject = ((black_actions.include?(payload[:action]) and !white_actions.include?(payload[:action])) or (black_actions == [] and !white_actions.include?(payload[:action])))         
+          end
+        end
+        unless reject
+          
+          payload[:forbidden_params].keys.each do |key|
+            forbidden_params = (search_hash(payload, key) == payload[:forbidden_params][key])
+            if forbidden_params
+              reject = forbidden_params
+              return  reject
+            end
+          end
+          
+        end
+      end
+      return reject
+    end
+
+    def search_hash(h, search)
+      return h[search] if h.fetch(search, false)
+    
+      h.keys.each do |k|
+        answer = search_hash(h[k], search) if h[k].is_a? Hash
+        return answer if answer
+      end
+    
+      false
+    end
+
 
     def redirect_to(event)
       Thread.current[:logstasher_location] = event.payload[:location]
@@ -93,42 +168,6 @@ module LogStasher
       end
     end
 
-    def extract_custom_fields(payload)
-      custom_fields = (!LogStasher.custom_fields.empty? && payload.extract!(*LogStasher.custom_fields)) || {}
-      LogStasher.custom_fields.clear
-      custom_fields
-    end
-  end
 
-  class MailerLogSubscriber < ActiveSupport::LogSubscriber
-    MAILER_FIELDS = [:mailer, :action, :message_id, :from, :to]
-
-    def deliver(event)
-      process_event(event, ['mailer', 'deliver'])
-    end
-
-    def receive(event)
-      process_event(event, ['mailer', 'receive'])
-    end
-
-    def process(event)
-      process_event(event, ['mailer', 'process'])
-    end
-
-    def logger
-      LogStasher.logger
-    end
-
-    private
-
-    def process_event(event, tags)
-      data = LogStasher.request_context.merge(extract_metadata(event.payload))
-      event = LogStash::Event.new('@source' => LogStasher.source, '@fields' => data, '@tags' => tags)
-      logger << event.to_json + "\n"
-    end
-
-    def extract_metadata(payload)
-      payload.slice(*MAILER_FIELDS)
-    end
   end
 end
