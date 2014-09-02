b485b9fdc579814836d9b5aeb0889a4bae66336f
alexphys <alexandros.mastas@gmail.com>
2014-09-02 15:46:01 +0300

    Update log_subscriber.rb

--- lib/logstasher/log_subscriber.rb
+++ lib/logstasher/log_subscriber.rb
@@ -1,6 +1,3 @@
-require 'active_support/core_ext/class/attribute'
-require 'active_support/log_subscriber'
-
 module LogStasher
   class RequestLogSubscriber < ActiveSupport::LogSubscriber
     def process_action(event)
@@ -48,8 +45,10 @@ module LogStasher
 
       white_list = payload[:white_list] || {method: [], controller: {}}  
       black_list = payload[:black_list] || {method: [], controller: {}}
+      #puts 'white list is ' + white_list.to_s
+      #puts 'black list is ' + black_list.to_s
       forbidden_params = payload[:forbidden_params] || {}
-  
+      #puts 'forbidden_params list is ' + forbidden_params.to_s  
       if black_list[:method].include?(payload[:method])
 
         if (white_list[:controller][payload[:controller]] && white_list[:controller][payload[:controller]][:actions].include?(payload[:action])) or (white_list[:controller][payload[:controller]] == {actions: []}) or white_list[:method].include?(payload[:method])
@@ -59,13 +58,15 @@ module LogStasher
           reject = true
         end
       end
+
       reject ||=false
 
-
+      #puts 'reject is after method ' + reject.to_s
       unless reject
 
 
         reject = black_list[:controller].keys.include?(payload[:controller]) and !white_list[:controller].keys.include?(payload[:controller])
+        #puts 'reject is after controller black check ' + reject.to_s
         if reject
           black_actions = black_list[:controller][payload[:controller]][:actions] || []
           white_list[:controller][payload[:controller]] ||= {}
@@ -75,9 +76,12 @@ module LogStasher
 
         unless reject
           
+          payload.delete(:forbidden_params)
           forbidden_params.keys.each do |key|
             found = (search_hash(payload, key) == forbidden_params[key])
+            #puts 'reject is after params ' + reject.to_s + ' setting is ' + forbidden_params.to_s + ' params is ' + payload.to_s
             if found
+
               reject = found
               return  reject
             end
@@ -85,6 +89,7 @@ module LogStasher
           
         end
       end
+      #puts 'reject final is  ' + reject.to_s
       return reject
     end
 
@@ -101,110 +106,6 @@ module LogStasher
 
 
 
-    def redirect_to(event)
-      Thread.current[:logstasher_location] = event.payload[:location]
-    end
 
-    private
-
-    def extract_request(payload)
-      {
-        :method => payload[:method],
-        :path => extract_path(payload),
-        :format => extract_format(payload),
-        :controller => payload[:params]['controller'],
-        :action => payload[:params]['action']
-      }
-    end
-
-    def extract_path(payload)
-      payload[:path].split("?").first
-    end
-
-    def extract_format(payload)
-      if ::ActionPack::VERSION::MAJOR == 3 && ::ActionPack::VERSION::MINOR == 0
-        payload[:formats].first
-      else
-        payload[:format]
-      end
-    end
-
-    def extract_status(payload)
-      if payload[:status]
-        { :status => payload[:status].to_i }
-      else
-        { :status => 0 }
-      end
-    end
-
-    def runtimes(event)
-      {
-        :duration => event.duration,
-        :view => event.payload[:view_runtime],
-        :db => event.payload[:db_runtime]
-      }.inject({}) do |runtimes, (name, runtime)|
-        runtimes[name] = runtime.to_f.round(2) if runtime
-        runtimes
-      end
-    end
-
-    def location(event)
-      if location = Thread.current[:logstasher_location]
-        Thread.current[:logstasher_location] = nil
-        { :location => location }
-      else
-        {}
-      end
-    end
-
-    # Monkey patching to enable exception logging
-    def extract_exception(payload)
-      if payload[:exception]
-        exception, message = payload[:exception]
-        status = ActionDispatch::ExceptionWrapper.status_code_for_exception(exception)
-        message = "#{exception}\n#{message}\n#{($!.backtrace.join("\n"))}"
-        { :status => status, :error => message }
-      else
-        {}
-      end
-    end
-
-    def extract_custom_fields(payload)
-      custom_fields = (!LogStasher.custom_fields.empty? && payload.extract!(*LogStasher.custom_fields)) || {}
-      LogStasher.custom_fields.clear
-      custom_fields
-    end
-  end
-
-  class MailerLogSubscriber < ActiveSupport::LogSubscriber
-    MAILER_FIELDS = [:mailer, :action, :message_id, :from, :to]
-
-    def deliver(event)
-      process_event(event, ['mailer', 'deliver'])
-    end
-
-    def receive(event)
-      process_event(event, ['mailer', 'receive'])
-    end
-
-    def process(event)
-      process_event(event, ['mailer', 'process'])
-    end
-
-    def logger
-      LogStasher.logger
-    end
-
-    private
-
-    def process_event(event, tags)
-      data = LogStasher.request_context.merge(extract_metadata(event.payload))
-      event = LogStash::Event.new('@source' => LogStasher.source, '@fields' => data, '@tags' => tags)
-      logger << event.to_json + "\n"
-    end
-
-    def extract_metadata(payload)
-      payload.slice(*MAILER_FIELDS)
-    end
   end
 end
